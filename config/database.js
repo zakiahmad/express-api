72abf1b6eaad9f97de522d9785df3ccb7608adc9
zakiahmad <ahmadmuzaki@gmail.com>
2022-08-07 19:08:50 +0700

    membuat file koneksi mysql

---
+++ config/database.js
@@ -0,0 +1,18 @@
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+	host:		'localhost',
+	user:		'', 
+	password:	'',
+ 	database:	'express_api'
+});
+
+connection.connect(function(error){
+	if(!!error){
+	  console.log(error);
+	}else{
+	  console.log('Connection successfull');
+	}
+})
+
+module.exports = connection;
