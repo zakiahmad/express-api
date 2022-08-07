c7e60de3f619f70ad1ddbe76498b70f95062a233
Ahmad Muzaki <ahmadmuzaki@gmail.com>
2022-08-07 07:17:54 +0700

    edit index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Belajar membuat REST API dengan Express js' });
 });
 
 module.exports = router;
